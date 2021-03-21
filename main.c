3ff6fe0b065eaae274f66081de5e92ab35e9dc1b
mokumus <rocketpolka@gmail.com>
2021-03-22 00:26:38 +0300

    init args skeleton

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>  
 #include <unistd.h>  
+#include <string.h>
 
+#define MAX_PATH 1024   // Input file path
+#define PERMS_LEN 10	// # of file permissions
 
 #define errExit(msg) do{ perror(msg); exit(EXIT_FAILURE); } while(0)
 
 // Function Prototypes
 void print_usage(void);
-
+int find_str(char* haystack, char* needle);
 
 int main(int argc, char* argv[])  {
 
-    char filename_regex[255],
-         perms[10],
+    char filename_regex[MAX_PATH],
+         perms[PERMS_LEN],
          fie_type = 'x';
     
     int option,
         size_bytes = -1,
         links = -1;
+
+    snprintf(filename_regex,MAX_PATH,"%s","---");
+    snprintf(perms,PERMS_LEN,"%s","---");
     
     while((option = getopt(argc, argv, "f:b:t:p:l:")) != -1){ //get option from the getopt() method
         switch(option){
@@ -28,13 +34,13 @@ int main(int argc, char* argv[])  {
                 fie_type = optarg[0];
                 break;
             case 'p':
-                snprintf(perms,10,"%s",optarg);
+                snprintf(perms,PERMS_LEN,"%s",optarg);
                 break;
             case 'l':
                 links = atoi(optarg);
                 break;
             case 'f':
-                snprintf(filename_regex,255,"%s",optarg);
+                snprintf(filename_regex,MAX_PATH,"%s",optarg);
                 break;
             default:
                 print_usage(); exit(EXIT_FAILURE);
@@ -52,6 +58,10 @@ int main(int argc, char* argv[])  {
            "Permissions(p): %s\n"
            "links(l):       %d %s\n"
            ,filename_regex, size_bytes,size_bytes == -1 ? "(Not specified)" : "", fie_type, perms,links, links == -1 ? "(Not specified)" : "");
+
+
+    printf("Perms Null?: %d\n", strlen(perms) <= 0);
+    find_str("char* haystack", filename_regex);
     
     return 0;
 }
@@ -67,6 +77,15 @@ void print_usage(void){
     printf("========================================\n");
 }
 
+
+int find_str(char* haystack, char* needles){
+	char *p = strtok(needles, "+");
+	while(p != NULL) {
+	    printf("%s\n", p);
+	    p = strtok(NULL, "+");
+	}
+	return 0;
+}
 
 
 
