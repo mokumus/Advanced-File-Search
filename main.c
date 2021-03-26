e82b50e6690a8e6870f8a4c1ff93c93cb7537973
mokumus <rocketpolka@gmail.com>
2021-03-26 08:56:56 +0300

    things

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>  
 #include <unistd.h>  
-#include <string.h>
 
-#define MAX_PATH 1024   // Input file path
+#define MAX_PATH 255   // Input file path
 #define PERMS_LEN 10	// # of file permissions
 
 #define errExit(msg) do{ perror(msg); exit(EXIT_FAILURE); } while(0)
 
 // Globals
-char _F[MAX_PATH],	// File name regex
+char _W[MAX_PATH],
+	 _F[MAX_PATH],	// File name regex
      _P[PERMS_LEN],	// File permissions
      _T = 'x';		// File type
 
@@ -17,7 +17,7 @@ char _F[MAX_PATH],	// File name regex
 int _B = -1,		// File size in bytes
     _L = -1;		// File links(#)
 
-int opt_B = 0, opt_T = 0, opt_P = 0, opt_L = 0, opt_F = 0;
+int opt_B = 0, opt_T = 0, opt_P = 0, opt_L = 0, opt_F = 0, opt_W;
 
 
 // Function Prototypes
@@ -27,16 +27,17 @@ void print_usage(void);
 void print_inputs(void);
 int input_exists();
 
+
+
 // Helper & Misc. functions
-int find_str(char* haystack, char* needle);
+void to_lower_case(char* str);
+int str_cmp(char* str1, char* str2);
+
 
 int main(int argc, char* argv[])  {
 	int option;
-
-    snprintf(_F,MAX_PATH,"%s","---");
-    snprintf(_P,PERMS_LEN,"%s","---");
     
-    while((option = getopt(argc, argv, "f:b:t:p:l:")) != -1){ //get option from the getopt() method
+    while((option = getopt(argc, argv, "f:b:t:p:l:w:")) != -1){ //get option from the getopt() method
         switch(option){
             case 'b':
             	opt_B = 1;
@@ -58,11 +59,22 @@ int main(int argc, char* argv[])  {
             	opt_F = 1;
                 snprintf(_F,MAX_PATH,"%s",optarg);
                 break;
+            case 'w':
+            	opt_W = 1;
+                snprintf(_W,MAX_PATH,"%s",optarg);
+                break;            
             default:
                 print_usage(); exit(EXIT_FAILURE);
         }
     }
       
+    if(opt_W == 0){
+    	printf("No search directory provided\n");
+    	print_usage();
+    	exit(EXIT_FAILURE);
+
+    }
+
     for(; optind < argc; optind++) //when some extra arguments are passed
         fprintf(stderr,"Given extra arguments: %s\n", argv[optind]);
     
@@ -73,22 +85,22 @@ int main(int argc, char* argv[])  {
     }
 
     print_inputs();
+    char str1[] = "mrAz";
+    char str2[] = "Mraz";
+    printf("result: %d\n", str_cmp(str1, str2));
 
-    
-    find_str("char* haystack", _F);
-    
     return 0;
 }
 
 void print_usage(void){
-    printf("========================================\n");
-    printf("Usage:\n"
-           "./myFind [-f filename] [-b file size(bytes)] [-t file type] [-p permissions] [-l # of _L]\n\n");
-    printf("-f : supporting the following regular expression(s): +\n"
-           "-t :     d: directory\n\t s: socket\n\t b: block device\n\t c: character device\n\t f: regular file\n\t p: pipe\n\t l: symbolic link\n"
-           "-p : 9 characters (e.g. ‘rwxr-xr--’)\n");
-  
-    printf("========================================\n");
+	char buffer[1024] = "========================================\n"
+						"Usage:\n"
+						"./myFind [-f filename] [-b file size(bytes)] [-t file type] [-p permissions] [-l # of _L] [-w search directory(required)]\n\n"
+						"-f : supporting the following regular expression(s): +\n"
+						"-t :     d: directory\n\t s: socket\n\t b: block device\n\t c: character device\n\t f: regular file\n\t p: pipe\n\t l: symbolic link\n"
+						"-p : 9 characters (e.g. ‘rwxr-xr--’)\n"
+						"========================================\n";
+	write(STDOUT_FILENO, buffer, 1024 );
 }
 
 void print_inputs(void){
@@ -103,26 +115,33 @@ void print_inputs(void){
 		printf("Permissions(p): %s\n", _P);
 	if(opt_L)
 		printf("links(l):       %d\n", _L);
-
-	return;
 }
 
 int input_exists(){
 	return opt_F || opt_B || opt_T || opt_P || opt_L;
 }
 
-int find_str(char* haystack, char* needles){
-	char *p = strtok(needles, "+");
-	while(p != NULL) {
-	    printf("%s\n", p);
-	    p = strtok(NULL, "+");
+int str_cmp(char* str1, char* str2){
+	to_lower_case(str1);
+	to_lower_case(str2);
+
+	while(*str1){
+		if(*str1 != *str2)
+			break;
+		str1++;
+		str2++;
 	}
-	return 0;
+	// ASCII difference
+    return *str1 - *str2;
 }
 
-
-
-
+void to_lower_case(char* str){
+	while(*str != '\0'){
+		if(*str >= 65 && *str<= 90)
+            *str = *str + 32;
+        str++;
+	}
+}
 
 
 
